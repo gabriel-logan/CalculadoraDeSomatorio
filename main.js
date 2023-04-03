43e62cc489f29526225ef453eb6eef90bcd1953e
Gabriel Logan <96627244+gabriel-logan@users.noreply.github.com>
2023-04-03 03:54:03 -0300

    Codigo pronto para usar

--- main.js
+++ main.js
@@ -1,13 +1,14 @@
+// Definição da classe Calculadora
 class Calculadora {
-    constructor() {
-        this.valorVasio = 0 // Valor vasio para iniciar o somatorio
-    }
-
+    // Método para calcular fatorial de um número
     fatorial(valorParaFatoriar) {
+        // Caso especial para fatorial de zero
         if (valorParaFatoriar === 0) {
             return 1; // Fatorial de zero é 1
         } else {
+            // Inicializa o resultado como 1
             let resultado = 1;
+            // Itera de 1 até o valorParaFatoriar, multiplicando o resultado em cada iteração
             for (let i = 1; i <= valorParaFatoriar; i++) {
                 resultado *= i;
             }
@@ -15,24 +16,37 @@ class Calculadora {
         }
     }
 
-    somatorio(x) { // Valor da variavel X se tiver
-        const n = 23 // Valor An para soma parcial
-        const inicioSomatorio = 0 // Valor inicial do somatorio | Atenção com
-            // indeterminações
+    // Método para calcular o somatório de uma série
+    somatorio(x, n, initSum, serie) { // Valor da variavel X se tiver
+        const inicioSomatorio = initSum // Valor inicial do somatorio | Atenção com indeterminações
+        let valorVasio = 0
 
+        // Itera de initSum até n, somando os valores da série para cada iteração
         for (let k = inicioSomatorio; k < n + 1; k++) {
-            const serie = 4 * ((-1) ** k) / (2 * k + 1) // Aqui você passa a função
-            this.valorVasio += serie // Soma a serie de acordo com o valor de n
+            valorVasio += eval(serie) // Soma a serie de acordo com o valor de n
         }
-        return this.valorVasio // Retorna a soma parcial da serie
+        return valorVasio // Retorna a soma parcial da serie
     }
 }
 
-const startCalculadora = new Calculadora() // Crie um executor da classe
+// Cria um objeto da classe Calculadora
+const startCalculadora = new Calculadora();
 
-const numeroPi = Math.PI // Numero Pi com varias casas decimais
+// Função para calcular e exibir o resultado do somatório
+function calcular() {
+    // Obtém os valores dos campos do formulário
+    const valorDeX = Number(document.getElementById("x").value);
+    const valorDeN = Number(document.getElementById("n").value);
+    const initSum = Number(document.getElementById("inicioSomatorio").value);
+    let valorDaSerie = document.getElementById("serie").value;
 
-// Os valores de x para angulos são apenas em radianos
-const valorDeX = numeroPi / 4
+    if (valorDeX < 0 || valorDeN < 0 || initSum < 0 || !valorDaSerie) {
+        return alert("Por favor, preencha todos os campos corretamente."); // Impede que o formulário seja enviado
+    }
 
-console.log(startCalculadora.somatorio(valorDeX))
+    // Calcula o resultado do somatório usando o objeto da classe Calculadora
+    const resultado = startCalculadora.somatorio(valorDeX, valorDeN, initSum, valorDaSerie);
+
+    // Exibe o resultado no elemento com id "resultado"
+    document.getElementById("resultado").innerHTML = 'Resultado: ' + resultado;
+}
